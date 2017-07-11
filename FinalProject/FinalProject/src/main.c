7bc4150176a2f198c380a6e9c20c460a5c262af9
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-07-11 14:11:08 -0400

    Added libraries

---
+++ FinalProject/FinalProject/src/main.c
@@ -0,0 +1,12 @@
+#include <asf.h>
+#include <Adafruit_Trellis.h>
+#include <delay.h>
+
+int main (void)
+{
+	/* Insert system clock initialization code here (sysclk_init()). */
+
+	board_init();
+
+	/* Insert application code here, after the board has been initialized. */
+}
