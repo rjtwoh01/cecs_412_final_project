fb63516410c5111c439b370e4223ef1670429ea7
DESKTOP-0GGS90G\illod <rjtwoh01@louisville.edu>
2017-07-14 11:15:11 -0400

    Created new FinalProject Atmel project
    This one actually compiles - will rebuild the library for the keypad and integrate that. Hopefully that won't cause any issues

---
+++ FinalProject/FinalProject/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\common\services\ioport\xmega\ioport_compat.c
+
+src\ASF\xmega\boards\xmega_a3bu_xplained\init.c
+
+src\main.c
+
