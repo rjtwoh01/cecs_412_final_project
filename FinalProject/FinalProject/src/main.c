64011e222a996c8d49badc9f72093e4ecfa5f87c
rjtwoh01 <rjtwoh01@cardmail.louisville.edu>
2017-07-17 14:13:32 -0400

    Working on recieving fingerprint

--- FinalProject/FinalProject/src/main.c
+++ FinalProject/FinalProject/src/main.c
@@ -1,27 +1,151 @@
-#include <asf.h>
 #include <board.h>
 #include <sysclk.h>
 #include <st7565r.h>
 #include <conf_usart_example.h>
 #include "led.h"
 
-int main (void)
+int getCharacter(int input);
+void displayCharacter(uint8_t character);
+void resetScreen();
+
+const int Characters[9] = {
+	0x30,
+	0x31,
+	0x32,
+	0x33,
+	0x34,
+	0x35,
+	0x36,
+	0x37,
+	0x38,
+	0x39
+};
+
+//! the page address to write to
+uint8_t page_address;
+//! the column address, or the X pixel.
+uint8_t column_address;
+//! store the LCD controller start draw line
+uint8_t start_line_address = 0;
+
+int main(void)
 {
 	board_init();
 	sysclk_init();
 
-	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT);
+	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT); //turns backlight on
 
+	// initialize the interface (SPI), ST7565R LCD controller and LCD
 	st7565r_init();
-	
-	//resetScreen();
-	
-	static usart_rs232_options USART_SERIAL_OPTIONS = {
+
+	// set addresses at beginning of display
+	resetScreen();
+
+	// USART options.
+	static usart_rs232_options_t USART_SERIAL_OPTIONS = {
 		.baudrate = USART_SERIAL_EXAMPLE_BAUDRATE,
 		.charlength = USART_SERIAL_CHAR_LENGTH,
 		.paritytype = USART_SERIAL_PARITY,
 		.stopbits = USART_SERIAL_STOP_BIT
-	}
+	};
 	
-	usart_init_rs232(USART_SERIAL_EXAMPLE);
+	// Initialize usart driver in RS232 mode
+	usart_init_rs232(USART_SERIAL_EXAMPLE, &USART_SERIAL_OPTIONS);
+
+	uint8_t tx_buf[] = "\n\rEnter a message: ";
+	int tx_length = 128;
+	int i;
+	for (i = 0; i < tx_length; i++) {
+		usart_putchar(USART_SERIAL_EXAMPLE, tx_buf[i]);
+	}
+
+	uint8_t input;
+	while (true) {
+		input = usart_getchar(USART_SERIAL_EXAMPLE);
+		int userInput = getCharacter(input);
+		displayCharacter(userInput);
+	}
+}
+
+int getCharacter(int input) {
+	char character = (char)input;
+	
+	int x;
+	switch(character){
+		case '30':
+			x = 1;
+			break;
+		case '31':
+			x = 1;
+			break;
+		case '32':
+		x = 2;
+		printf("x = %d", &x);
+		break;
+		case '33':
+		x = 3;
+		break;
+		case '34':
+		x = 4;
+		break;
+		case '35':
+		x = 5;
+		break;
+		case '36':
+		x = 6;
+		break;
+		case '37':
+		x = 7;
+		break;
+		case '38':
+		x = 8;
+		break;
+		case '39':
+		x = 9;
+		break;
+	}
+	return x;
+}
+
+void displayCharacter(uint8_t character)
+{
+	// set addresses at beginning of display
+	gpio_set_pin_low(NHD_C12832A1Z_BACKLIGHT); //turns backlight off
+
+	if (character == 37) {
+		start_line_address += 7;
+		st7565r_set_display_start_line_address(start_line_address++);
+		st7565r_set_column_address(0);
+		st7565r_set_page_address(++page_address);
+	}
+
+	int i;
+	if (character == 100) {
+		resetScreen();
+	}
+	else {
+		st7565r_write_data(Characters[character]);
+	}
+	delay_ms(100);
+	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT); //turns backlight on
+}
+
+void resetScreen()
+{
+	int counter = 0;
+	// clear display
+	for (page_address = 0; page_address <= 4; page_address++) {
+		st7565r_set_page_address(page_address);
+		for (column_address = 0; column_address < 128; column_address++) {
+			st7565r_set_column_address(column_address);
+			/* fill every other pixel in the display. This will produce
+			horizontal lines on the display. */
+			st7565r_write_data(0x00);
+			if (counter >= 512)
+				break;
+		}
+	}
+
+	st7565r_set_page_address(0);
+	st7565r_set_column_address(0);
 }
--- fingerprint/fingerprint.ino
+++ fingerprint/fingerprint.ino
@@ -47,7 +47,7 @@ Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 void setup()  
 {
   Serial.begin(9600);
-  Serial.println("fingertest");
+  //Serial.println("fingertest");
 
   // set the data rate for the sensor serial port
   finger.begin(57600);
@@ -55,20 +55,20 @@ void setup()
    //pinMode(1,OUTPUT); //Set PIN 1 to output
   
   if (finger.verifyPassword()) {
-    Serial.println("Found fingerprint sensor!");
+    //Serial.println("Found fingerprint sensor!");
   } else {
-    Serial.println("Did not find fingerprint sensor :(");
+    //Serial.println("Did not find fingerprint sensor :(");
     while (1);
   }
-  Serial.println("Waiting for valid finger...");
+  //Serial.println("Waiting for valid finger...");
 
   pinMode(rx,INPUT);
   pinMode(tx,OUTPUT);
   digitalWrite(tx,HIGH);
   digitalWrite(13,HIGH); //turn on debugging LED
-  SWprint('h');  //debugging hello
-  SWprint('i');
-  SWprint(10); //carriage return
+  //SWprint('h');  //debugging hello
+  //SWprint('i');
+  //SWprint(10); //carriage return
 }
 
 void SWprint(int data)
@@ -119,19 +119,19 @@ uint8_t getFingerprintID() {
   uint8_t p = finger.getImage();
   switch (p) {
     case FINGERPRINT_OK:
-      Serial.println("Image taken");
+      //Serial.println("Image taken");
       break;
     case FINGERPRINT_NOFINGER:
-      Serial.println("No finger detected");
+      //Serial.println("No finger detected");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
+      //Serial.println("Communication error");
       return p;
     case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
+      //Serial.println("Imaging error");
       return p;
     default:
-      Serial.println("Unknown error");
+      //Serial.println("Unknown error");
       return p;
   }
 
@@ -140,43 +140,43 @@ uint8_t getFingerprintID() {
   p = finger.image2Tz();
   switch (p) {
     case FINGERPRINT_OK:
-      Serial.println("Image converted");
+      //Serial.println("Image converted");
       break;
     case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
+      //Serial.println("Image too messy");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
+      //Serial.println("Communication error");
       return p;
     case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
+      //Serial.println("Could not find fingerprint features");
       return p;
     case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
+      //Serial.println("Could not find fingerprint features");
       return p;
     default:
-      Serial.println("Unknown error");
+      //Serial.println("Unknown error");
       return p;
   }
   
   // OK converted!
   p = finger.fingerFastSearch();
   if (p == FINGERPRINT_OK) {
-    Serial.println("Found a print match!");
+    //Serial.println("Found a print match!");
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    Serial.println("Communication error");
+    //Serial.println("Communication error");
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
-    Serial.println("Did not find a match");
+    //Serial.println("Did not find a match");
     return p;
   } else {
-    Serial.println("Unknown error");
+    //Serial.println("Unknown error");
     return p;
   }   
   
   // found a match!
-  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
-  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+  //Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  //Serial.print(" with confidence of "); Serial.println(finger.confidence); 
 }
 
 // returns -1 if failed, otherwise returns ID #
@@ -191,8 +191,8 @@ int getFingerprintIDez() {
   if (p != FINGERPRINT_OK)  return -1;
   
   // found a match!
-  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
-  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  /*Serial.print("");*/ Serial.print(finger.fingerID); 
+  //Serial.print(" with confidence of "); Serial.println(finger.confidence);
   sendSignal(finger.fingerID);
   I2CSignal(finger.fingerID);
   return finger.fingerID; 
@@ -202,7 +202,7 @@ void sendSignal(int fingerID) {
   //Send a signal saying who the match is
 
   //One or the other I think
-  Serial.write(fingerID);
+  //Serial.write(fingerID);
   //Serial.println(fingerID);
   //sendSerial.write(fingerID);
   //Serial.write("H");
@@ -211,6 +211,6 @@ void sendSignal(int fingerID) {
 }
 
 void I2CSignal(int fingerID) {
-  SWprint(fingerID);
+  //SWprint(fingerID);
   //Wire.beginTransmission(
 }
